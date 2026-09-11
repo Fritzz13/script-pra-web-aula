46ef2f4d4fa9502b620199452008a8d6c9062b8c
Fritzz <tarcisiofalb@gmail.com>
2026-09-10 21:30:51 -0300

    adicao do 'link clicker'

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const themebutton = document.querySelector("#themeButton");
+const links = document.querySelectorAll(".navLink");
+const counterp = document.querySelector("#clicksLabel");
+let total = 0;
+
 themebutton.addEventListener( "click", function () { document.body.classList.toggle("modoClaro"); } );
+
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    total++;
+    counterp.innerHTML = `Clicks: ${total}`;
+    counterp.classList.add("shown");
+  } );
+} );

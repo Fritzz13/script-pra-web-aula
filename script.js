2e2bec3d275fcbbdd2e5e2b6b24570751692711c
Fritzz <tarcisiofalb@gmail.com>
2026-09-10 21:15:29 -0300

    adicao do modo claro

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const themebutton = document.querySelector("#themeButton");
-themebutton.addEventListener( "click", function () { document.body.classList.toggle("escuro"); } );
+themebutton.addEventListener( "click", function () { document.body.classList.toggle("modoClaro"); } );

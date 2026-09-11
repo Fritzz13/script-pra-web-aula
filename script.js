77a21c02cb372683360fde430b715ffdda3a7c36
Fritzz <tarcisiofalb@gmail.com>
2026-09-10 22:13:00 -0300

    adicao da validacao do form

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const themebutton = document.querySelector("#themeButton");
 const links = document.querySelectorAll(".navLink");
 const counterp = document.querySelector("#clicksLabel");
+const form = document.querySelector("form");
 let total = 0;
 
-themebutton.addEventListener( "click", function () { document.body.classList.toggle("modoClaro"); } );
+
+themebutton.addEventListener( "click", function () { document.body.classList.toggle("escuro"); } );
+
 
 links.forEach(function (link) {
   link.addEventListener("click", function () {
@@ -11,4 +14,27 @@ links.forEach(function (link) {
     counterp.innerHTML = `Clicks: ${total}`;
     counterp.classList.add("shown");
   } );
+} );
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const campos = form.querySelectorAll("input");
+  const formbutton = form.querySelector("#formButton");
+  let vazio = false;
+  
+  formbutton.innerHTML = "Enviar";
+  
+  campos.forEach(function (campo) {
+    campo.value.trim();
+    if (campo.value === '') { vazio = true; }
+  } );
+
+  if (vazio) {
+    alert("Por favor, preencha o formulário completo antes de tentar enviar.");
+  } else {
+    campos.forEach(function (campo) { campo.value = ''; } );
+    formbutton.innerHTML = "Sugestão enviada!";
+    setTimeout(function () { formbutton.innerHTML = "Enviar"; }, 1300);
+  }
 } );

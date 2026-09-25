97a75e1c617f7c5f57f8367504720d4e29eeba1d
Fritzz <tarcisiofalb@gmail.com>
2026-09-25 20:05:59 -0300

    3a questao feita

---
+++ lista 11-09/questao3.js
@@ -0,0 +1,7 @@
+const nome = prompt("Informe o seu nome: ");
+const idade = Number(prompt("Informe a sua idade: "));
+const altura = Number(prompt("Informe a sua altura: "));
+
+alert(`typeof(${nome}) = ${typeof(nome)}
+typeof(${idade}) = ${typeof(idade)}
+typeof(${altura}) = ${typeof(altura)}`);

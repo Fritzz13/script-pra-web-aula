2fa035b56488667b7e378395dc275c84251111e0
Fritzz <tarcisiofalb@gmail.com>
2026-09-25 20:11:14 -0300

    4a questao feita

---
+++ lista 11-09/questao4.js
@@ -0,0 +1,9 @@
+const num1 = Number(prompt("Informe um numero: "));
+const num2 = Number(prompt("Informe outro numero: "));
+
+alert(`${num1} == ${num2} -> ${num1 == num2}
+${num1} != ${num2} -> ${num1 != num2}
+${num1} > ${num2} -> ${num1 > num2}
+${num1} < ${num2} -> ${num1 < num2}
+${num1} === ${num2} -> ${num1 === num2}
+${num1} !== ${num2} -> ${num1 !== num2}`)

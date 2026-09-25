99b045a4ac96f1aea890b5ce77c80ce2df5cf4e7
Fritzz <tarcisiofalb@gmail.com>
2026-09-25 19:59:01 -0300

    2a questao feita

---
+++ lista 11-09/questao2.js
@@ -0,0 +1,9 @@
+const num1 = Number(prompt("Informe um número: "));
+const num2 = Number(prompt("Informe outro número: "));
+
+alert(`${num1} + ${num2} = ${num1 + num2}
+${num1} - ${num2} = ${num1 - num2}
+${num1} * ${num2} = ${num1 * num2}
+${num1} / ${num2} = ${num1 / num2}
+${num1} % ${num2} = ${num1 % num2}
+${num1}^${num2} = ${num1**num2}`);

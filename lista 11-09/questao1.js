70d919f779737dba43d575cde1480a285357169d
Fritzz <tarcisiofalb@gmail.com>
2026-09-25 19:46:44 -0300

    inicio da primeira lista, 1a questao feita

---
+++ lista 11-09/questao1.js
@@ -0,0 +1,6 @@
+const nome = prompt("Informe o seu nome: ");
+const idade = prompt("Informe a sua idade: ");
+const curso = prompt("Informe o seu curso: ");
+const cidade = prompt("Informe a sua cidade: ");
+
+alert(`Olá ${nome}! É muito bom ver que você veio de ${cidade} para fazer o curso de ${curso}! \nvoce tem ${idade} anos btw`);
